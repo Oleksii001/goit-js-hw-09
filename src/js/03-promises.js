3afb0abf2c1b8e965af210b5c4c3baa680de0355
Oleksii001 <oleksii.burlaka@gmail.com>
2023-06-04 11:29:38 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,4 +1,4 @@
-function createPromise(position, delay) {
+    function createPromise(position, delay) {
       return new Promise((resolve, reject) => {
         const shouldResolve = Math.random() > 0.3;
         setTimeout(() => {
@@ -11,12 +11,13 @@ function createPromise(position, delay) {
       });
     }
 
-    document.querySelector('.form').addEventListener('submit', function (event) {
+    document.querySelector('.form').addEventListener('submit', (event) => {
       event.preventDefault();
 
-      const delayInput = this.elements['delay'];
-      const stepInput = this.elements['step'];
-      const amountInput = this.elements['amount'];
+      const delayInput = document.querySelector('input[name="delay"]');
+      const stepInput = document.querySelector('input[name="step"]');
+      const amountInput = document.querySelector('input[name="amount"]');
+      const resultsDiv = document.getElementById('results');
 
       const firstDelay = parseInt(delayInput.value);
       const step = parseInt(stepInput.value);
@@ -26,12 +27,68 @@ function createPromise(position, delay) {
       for (let i = 1; i <= amount; i++) {
         createPromise(i, delay)
           .then(({ position, delay }) => {
-            console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+            const result = document.createElement('p');
+            result.textContent = `✅ Fulfilled promise ${position} in ${delay}ms`;
+            resultsDiv.appendChild(result);
+
+            setTimeout(() => {
+              result.remove();
+            }, 4000);
           })
           .catch(({ position, delay }) => {
-            console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+            const result = document.createElement('p');
+            result.textContent = `❌ Rejected promise ${position} in ${delay}ms`;
+            resultsDiv.appendChild(result);
+
+            setTimeout(() => {
+              result.remove();
+            }, 10000);
           });
 
         delay += step;
       }
     });
+
+
+
+
+    
+//  function createPromise(position, delay) {
+//       return new Promise((resolve, reject) => {
+//         const shouldResolve = Math.random() > 0.3;
+//         setTimeout(() => {
+//           if (shouldResolve) {
+//             resolve({ position, delay });
+//           } else {
+//             reject({ position, delay });
+//           }
+//         }, delay);
+//       });
+//     }
+
+//     document.querySelector('.form').addEventListener('submit', function (event) {
+//       event.preventDefault();
+
+//       const delayInput = this.elements['delay'];
+//       const stepInput = this.elements['step'];
+//       const amountInput = this.elements['amount'];
+
+//       const firstDelay = parseInt(delayInput.value);
+//       const step = parseInt(stepInput.value);
+//       const amount = parseInt(amountInput.value);
+
+//       let delay = firstDelay;
+//       for (let i = 1; i <= amount; i++) {
+//         createPromise(i, delay)
+//           .then(({ position, delay }) => {
+//             console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//           })
+//           .catch(({ position, delay }) => {
+//             console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//           });
+
+//         delay += step;
+//       }
+//     });
+
+
